6e551b87737f6d13dde8de3c1d317daff9823552
21327 <rodrigo.villegaszambrano@vtsd.com>
2022-09-12 07:59:29 -0400

    text

---
+++ DataTypeExamples.java
@@ -0,0 +1,16 @@
+// Creates a single line comment
+/*
+    This is a multi-line comment.
+    All the lines between the slashes and stars will eb ignored.
+ */
+
+public class DataTypeExamples {
+    public static void main(String[] args) {
+        //Variable syntax: dataTypes varName = value
+        int age = 200;
+        // Character values must use a single quote
+        char favLetter = 'Z';
+        // String values must use double quote
+        String bestGame = "chess";
+    }
+}
--- OurFirstClass.java
+++ OurFirstClass.java
@@ -7,4 +7,8 @@ With great power comes great responsibility - Uncle Ben (Many Universes)
 
 
 public class OurFirstClass {
+    public static void main(String args[]) {
+        System.out.print("Hello World!");
+    }
+
 }
